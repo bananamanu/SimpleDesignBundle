0f22b3cfdc92249ea54417dc20e3326ac04e6592
bananamanu <ebichet@sqli.com>
2015-07-20 17:32:15 +0200

    Add active location on top menu

--- Controller/MenuController.php
+++ Controller/MenuController.php
@@ -8,10 +8,12 @@ use eZ\Bundle\EzPublishCoreBundle\Controller;
 class MenuController extends Controller
 {
     /**
+     * Controller  for top menu
+     * @param int $currentLocationId
      *
      * @return Response
      */
-    public function topMenuAction()
+    public function topMenuAction( $currentLocationId )
     {
         $rootLocationId = $this->getConfigResolver()->getParameter( 'content.tree_root.location_id' );
 
@@ -21,7 +23,27 @@ class MenuController extends Controller
         $helper = $this->get( 'bananamanu_simple_design.subelement_helper' );
         $menu = $helper->getSubElementLocation( $rootLocationId );
 
+        // Get current master
+        try
+        {
+            $currentLocation = $this->getRepository()->getLocationService()->loadLocation($currentLocationId);
+            $pathString = explode( '/', $currentLocation->pathString );
+            if ( isset( $pathString[3] ) )
+            {
+                $currentMasterLocationId = $pathString[3];
+            }
+            else
+            {
+                $currentMasterLocationId = $rootLocationId;
+            }
+        }
+        catch (\Exception $e)
+        {
+            $currentMasterLocationId = $rootLocationId;
+        }
+
         $parameters['menu'] = $menu;
+        $parameters['currentMasterLocationId'] = $currentMasterLocationId;
         return $this->render( 'BananamanuSimpleDesignBundle::page_topmenu.html.twig', $parameters, $response );
     }
 
