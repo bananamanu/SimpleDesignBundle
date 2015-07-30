f508fd6d3c4af110feca7c69e1f097709fb33f9d
bananamanu <bichetemmanuel@gmail.com>
2015-07-30 07:32:51 +0200

    Add controller to master full view to display subelement

---
+++ Controller/MasterController.php
@@ -0,0 +1,36 @@
+<?php
+namespace Bananamanu\SimpleDesignBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use eZ\Bundle\EzPublishCoreBundle\Controller;
+
+class MasterController extends Controller
+{
+
+    /**
+     * Renders master location in full view
+     *
+     * @param $locationId
+     * @param $viewType
+     * @param bool $layout
+     * @param array $params
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function viewLocationFullAction( $locationId, $viewType, $layout = false, array $params = array() )
+    {
+        // Get children content location
+        //@TODO include sort order
+        $helper = $this->get( 'bananamanu_simple_design.subelement_helper' );
+        $children = $helper->getSubElementLocation( $locationId );
+        $params['children'] = $children;
+
+        return $this->get( 'ez_content' )->viewLocation(
+            $locationId,
+            $viewType,
+            $layout,
+            $params
+        );
+    }
+
+}
